0cb07243af5e279462703e910a4c37d4465c277a
Aluko Ayomide <alukoayomide623@gmail.com>
2022-06-13 17:56:11 +0100

    another commit

---
+++ I4G000833EDW/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User=get_user_model()
+
+# Create your models here.
+class Post(models.Model):
+    Title = models.CharField(max_length=200)
+    Text = models.TextField()
+    Tuthor = models.ForeignKey(User, on_delete=models.CASCADE)
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
